9c8022ba84a4ee55853dc69d0f3d26bd4f6adfa0
Daniel Stankevich <standeo@gmail.com>
2015-03-09 04:01:31 +1100

    Little place spec

--- test/robot_spec.js
+++ test/robot_spec.js
@@ -1,7 +1,28 @@
 describe('robot', () => {
 
+  var subject, expected;
+  var position, robot, grid;
+
+  function _s(o) {
+    return JSON.stringify(o);
+  };
+
   it('is alive!', () => {
     expect(true).toBe(true);
+  });
+
+  describe('#place', () => {
+    beforeEach(() => {
+      position = new Position(2, 2);
+      robot    = new Robot(position, 0);
+      grid     = new Grid(5, 5, robot);
+      subject = place(grid, position, 0);
+    });
+
+    it('places a robot at a new position facing a new direction', () => {
+      expected = new Grid(5, 5, new Robot(new Position(2, 2), 0));
+      expect(_s(subject)).toEqual(_s(expected));
+    });
   });
 
 });
