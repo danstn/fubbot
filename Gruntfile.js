56311dc56962f8fd7367c6789e9c59e20a518e07
Daniel Stankevich <daniel.stankevich@rea-group.com>
2015-03-19 13:48:39 +1100

    Getting rid of phantom - WIP

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,6 @@
+process.env.NODE_PATH = __dirname + '/src';
+require('module').Module._initPaths();
+
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -26,5 +29,29 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['babel', 'karma']);
   grunt.registerTask('build', ['babel']);
   grunt.registerTask('test', ['karma']);
+
+  grunt.registerTask('specs', function() {
+    require('babel/register')();
+    var Jasmine = require('./node_modules/jasmine/lib/jasmine.js');
+    var jasmine = new Jasmine();
+
+    process.env.NODE_PATH = process.env.NODE_PATH + ":" + __dirname + '/test';
+    require('module').Module._initPaths();
+
+    jasmine.loadConfigFile(process.env.JASMINE_CONFIG_PATH);
+    jasmine.configureDefaultReporter({
+      showColors: true,
+      onComplete: this.async()
+    });
+
+    var reporters = require('jasmine-reporters');
+    var junitReporter = new reporters.JUnitXmlReporter({
+        savePath: __dirname + '/tmp/test/reports/',
+        consolidateAll: true
+    });
+    jasmine.addReporter(junitReporter);
+
+    jasmine.execute();
+  })
 };
 
